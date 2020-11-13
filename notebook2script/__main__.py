2dc76de800a64fa259754f31d1cc36db1ae975e8
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-13 16:10:14 +0000

    Exit with status code.

--- notebook2script/__main__.py
+++ notebook2script/__main__.py
@@ -25,7 +25,6 @@ import glob
 import os
 import pathlib
 import sys
-import warnings
 from typing import Iterable, Union
 
 # 3rd party
@@ -40,7 +39,7 @@ __all__ = ["main", "process_multiple_notebooks", "process_notebook"]
 linter = Pointless()
 
 
-def main() -> None:
+def main() -> int:
 
 	parser = argparse.ArgumentParser(description="Convert Jupyter Notebooks to Python scripts")
 
@@ -63,20 +62,19 @@ def main() -> None:
 	if not notebooks:
 		parser.error("the following arguments are required: NOTEBOOK")
 
-	process_multiple_notebooks(notebooks, args.outdir, overwrite=args.overwrite)
+	return process_multiple_notebooks(notebooks, args.outdir, overwrite=args.overwrite)
 
 
 def process_multiple_notebooks(
 		notebooks: Iterable[Union[str, pathlib.Path]],
 		outdir: Union[str, pathlib.Path, os.PathLike],
 		overwrite: bool = False,
-		) -> None:
+		) -> int:
 	"""
 
 	:param notebooks: An iterable of notebook filenames to process
 	:param outdir: The directory to store the Python output in.
 	:param overwrite: Whether to overwrite existing files. Default :py:obj:`False`
-	:type overwrite: bool
 	"""
 
 	if not isinstance(outdir, pathlib.Path):
@@ -90,18 +88,23 @@ def process_multiple_notebooks(
 	# print(all_notebooks)
 	# input(">")
 
+	ret = 0
+
 	for notebook in all_notebooks:
 		notebook = pathlib.Path(notebook)
 		outfile = outdir / f"{notebook.stem}.py"
 
 		if outfile.is_file() and not overwrite:
-			warnings.warn(f"Skipping existing file {outfile}")
+			print(f"Info: Skipping existing file {outfile}")
 		else:
 			if notebook.is_file():
 				print(f"Converting {notebook} to {outfile}")
 				process_notebook(notebook, outfile)
 			else:
 				print(f"{notebook} not found")
+				ret |= 1
+
+	return ret
 
 
 def process_notebook(notebook, outfile: Union[str, pathlib.Path, os.PathLike]) -> None:
@@ -119,6 +122,6 @@ def process_notebook(notebook, outfile: Union[str, pathlib.Path, os.PathLike]) -
 
 if __name__ == "__main__":
 	try:
-		main()
+		sys.exit(main())
 	except KeyboardInterrupt:
 		sys.exit(1)
