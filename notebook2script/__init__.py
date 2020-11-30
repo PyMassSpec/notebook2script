f0ddab4608526be6ab5c1751e4b0427615870aeb
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-30 13:28:31 +0000

    Bump version v0.1.0 -> v0.2.0

--- notebook2script/__init__.py
+++ notebook2script/__init__.py
@@ -42,7 +42,7 @@ from notebook2script.pointless import Pointless
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "GPLv2"
-__version__: str = "0.1.0"
+__version__: str = "0.2.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = [
